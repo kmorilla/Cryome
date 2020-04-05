0485105863c90a82d932055b1787888948432ec1
Kevyn Morilla <37521213+kmorilla@users.noreply.github.com>
2020-04-04 21:54:23 -0700

    Added the cosmetics section

--- js/main.js
+++ js/main.js
@@ -1,16 +1,31 @@
 
 function setHeight() {
 
-    //--- .box margin ---
+    //--- services margins ---
 
     var totalWidth = $(".box").outerWidth(true);
     var shortWidth = $(".box").width();
     var marginHeight = (totalWidth - shortWidth) / 2;
 
-    $(".box").css({ marginTop: marginHeight, marginBottom: marginHeight });
+    var marginTB = { marginTop: marginHeight, marginBottom: marginHeight };
+    var marginLR = { marginLeft: marginHeight, marginRight: marginHeight };
 
-    $(".therapy-title-container").css({ marginTop: marginHeight, marginBottom: marginHeight });
+    $(".box").css(marginTB);
 
+    var windowWidth = $(window).outerWidth();
+    var marginZeroTB = { marginTop: 0, marginBottom: 0 };
+    var marginZeroLR = { marginLeft: 0, marginRight: 0 };
+
+    if (windowWidth > 576) {
+        $(".therapy-title-container").css(marginTB);
+        $(".diagonal-stripes").css(marginLR);
+        $(".diagonal-stripes").css(marginTB);
+    }
+    else {
+        $(".therapy-title-container").css(marginZeroTB);
+        $(".diagonal-stripes").css(marginZeroLR);
+        $(".diagonal-stripes").css(marginZeroTB);
+    }
 
     //--- .box height ---
 
@@ -22,16 +37,15 @@ function setHeight() {
     //--- diagonal-stripes ---
     var boxHeight = $(".box").outerHeight(true);
 
-    console.log(boxHeight);
-
-
-    $(".diagonal-stripes").height(boxHeight * 1.6);
-    $(".diagonal-stripes").width(boxHeight * 1.6);
+    $(".diagonal-stripes").height(boxHeight * 1.5);
+    $(".diagonal-stripes").width(boxHeight * 1.5);
 }
+
 
 $(document).ready(function () {
     setHeight();
 })
+
 
 $(window).resize(function () {
     setHeight();
